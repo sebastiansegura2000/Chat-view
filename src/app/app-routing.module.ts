532b0c66b4380667bc3423de4d8cc9e85984149f
sebastiansegura2000 <sebastiansegura260@gmial.com>
2024-04-29 09:13:49 -0500

    creation of all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { GroupInfoComponent } from './group-info/group-info.component';
+import { MessageHistoryComponent } from './message-history/message-history.component';
+import { ProfileInfoComponent } from './profile-info/profile-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'chat', component: ChatComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'group-info', component: GroupInfoComponent },
+  { path: 'message-history', component: MessageHistoryComponent },
+  { path: 'profile-info', component: ProfileInfoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ChatComponent } from './chat/chat.component';
+import { MessageHistoryComponent } from './message-history/message-history.component';
+import { GroupInfoComponent } from './group-info/group-info.component';
+import { ProfileInfoComponent } from './profile-info/profile-info.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ChatComponent,
+    MessageHistoryComponent,
+    GroupInfoComponent,
+    ProfileInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
