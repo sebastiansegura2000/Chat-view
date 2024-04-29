d0e5a66e9189b16fdd0e70d418cec69c6bd91d96
juanmac09 <mateoromero0910@gmail.com>
2024-04-29 16:15:45 -0500

    frontend connection with backend

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../config.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent {
   showActionMenu: boolean = false;
+  data: any = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    this.apiService.getDataWithHeaders('http://127.0.0.1:80/api/group/get')
+      .subscribe({
+        next: (response) => {
+          this.data = response;
+        },
+        error: (error) => {
+          console.error('Error loading data:', error);
+        }
+      });
+    }
   toggleActionMenu() {
     this.showActionMenu = !this.showActionMenu;
+    console.log(this.data);
   }
 }
