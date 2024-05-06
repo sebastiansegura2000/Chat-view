2b7ea4b202c6465aa2a546196d2da686d8d8b8eb
juanmac09 <mateoromero0910@gmail.com>
2024-05-06 10:24:46 -0500

    added getting chats

--- src/app/Components/chat/chat.component.ts
+++ src/app/Components/chat/chat.component.ts
@@ -1,4 +1,3 @@
-// import { Component } from '@angular/core';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/Interfaces/User/user.interface';
@@ -15,30 +14,35 @@ import { Message } from 'src/app/Interfaces/Message/message.inteface';
 })
 export class ChatComponent implements OnInit {
   recipient: User;
-  sender:User;
-  messages:Message;
+  sender: User;
+  messages: Message;
   private routeSub: Subscription;
   showActionMenu: boolean = false;
+  sentMessages: { text: string; time: string }[] = [];
   constructor(
     private routerNavegation: Router,
     private route: ActivatedRoute,
     private userRepository: IUSerRepositoryService,
-    private messageService:IMessageQueryForUserService,
+    private messageService: IMessageQueryForUserService,
     private globalService: GlobalVariablesService
   ) {}
 
   ngOnInit(): void {
-    this.routeSuscribe();
+    this.route.params.subscribe((params) => {
+      const userId = params['id'];
+      this.getRecipient(userId);
+    });
     this.sender = this.globalService.userAuth.value.userData;
-    console.log(this.sender);
   }
-
+  /**
+   * Toggles the visibility of the action menu.
+   */
   toggleActionMenu() {
     this.showActionMenu = !this.showActionMenu;
   }
-
-  sentMessages: { text: string; time: string }[] = [];
-
+  /**
+   * Sends a message to the recipient.
+   */
   sendMessage() {
     const message = (
       document.querySelector('.type_msg') as HTMLInputElement
@@ -56,33 +60,60 @@ export class ChatComponent implements OnInit {
       }, 0);
     }
   }
-
-  // sendMessage() {
-  //   const message = (document.querySelector('.type_msg') as HTMLInputElement).value;
-  //   console.log(message)
-  //   (document.querySelector('.type_msg') as HTMLInputElement).value = '';
-  // }
-
+  /**
+   * Retrieves the recipient user based on the provided user ID.
+   *
+   * @param {number} user_id - The unique identifier of the recipient user.
+   */
   getRecipient(user_id) {
     this.userRepository.getUserForId(user_id).subscribe((response) => {
       this.recipient = response.user;
-      this.messageService.getMessage(this.recipient.id).subscribe((response) => {
-        this.messages = response.messages;
-        console.log(response);
-      });
+      this.loadMessage();
     });
   }
-
-  routeSuscribe(){
-    this.routeSub = this.route.params.subscribe((params) => {
-      const userId = params['id'];
-      this.getRecipient(userId);
+  /**
+   * Loads the messages for the current recipient.
+   */
+  loadMessage() {
+    this.messageService.getMessage(this.recipient.id).subscribe((response) => {
+      this.messages = response.messages;
     });
   }
+  /**
+   * Retrieves the time at which a message was created, formatted according to the current date and time.
+   *
+   * @param {string | Date} created_at - The timestamp of the message creation.
+   * @returns {string} The formatted time string.
+   */
+  getMessageTime(created_at: string | Date): string {
+    const messageDate = new Date(created_at);
+    const today = new Date();
 
-
-
-
+    if (
+      messageDate.getDate() === today.getDate() &&
+      messageDate.getMonth() === today.getMonth() &&
+      messageDate.getFullYear() === today.getFullYear()
+    ) {
+      const hour = messageDate.getHours() % 12 || 12;
+      const minutes = messageDate.getMinutes();
+      const ampm = messageDate.getHours() >= 12 ? 'PM' : 'AM';
+      return `${hour}:${minutes.toString().padStart(2, '0')} ${ampm}`;
+    } else {
+      const month = messageDate.toLocaleString('default', { month: 'short' });
+      const day = messageDate.getDate();
+      const hour = messageDate.getHours() % 12 || 12;
+      const minutes = messageDate.getMinutes();
+      const ampm = messageDate.getHours() >= 12 ? 'PM' : 'AM';
+      return `${day} ${month}, ${hour}:${minutes
+        .toString()
+        .padStart(2, '0')} ${ampm}`;
+    }
+  }
+  /**
+   * Listens for the Escape key press event and navigates back to the default chat page.
+   *
+   * @param {KeyboardEvent} event - The keyboard event object.
+   */
   @HostListener('document:keydown.escape', ['$event'])
   handleEscapeKey(event: KeyboardEvent) {
     this.routerNavegation.navigate(['/chat-def']);
